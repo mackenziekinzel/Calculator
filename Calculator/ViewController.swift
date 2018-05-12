9ce1358b411c89f6751eac2f555ac3619f753fb8
Mackenzie Kinzel <mkinzel2@kent.edu>
2018-05-12 15:56:45 -0400

    create mathematicalSymbol switch

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet var display: UILabel! = nil
     
     var userIsInTheMiddleOfTyping = false
-
+    
     @IBAction func touchDigit(_ sender: UIButton) {
         
         let digit = sender.currentTitle!
@@ -45,14 +45,22 @@ class ViewController: UIViewController {
             brain.setOperand(displayValue)
             userIsInTheMiddleOfTyping = false
         }
+        
         if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol{
+            case "π":
+                display!.text = "3.1415926"
+            default:
+                break
+            }
+            
             brain.performOperation(mathematicalSymbol)
         }
         
         if let result = brain.result {
             displayValue = result
         }
-
+        
     }
     
 }
